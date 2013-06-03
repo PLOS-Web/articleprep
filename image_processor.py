cfb9b789ad6a52affbf3ccf057289ba363fe515e
Vera Yin <verayin@gmail.com>
2013-06-02 21:41:23 -0700

    added exception handling

--- image_processor.py
+++ image_processor.py
@@ -2,11 +2,14 @@
 # usage: image_processor.py image1 image2 ...
 
 import sys
-import subprocess
+import subprocess as sp
 
 def call(command):
-    call = subprocess.Popen(command.split(), stdout = subprocess.PIPE, shell = False)
-    return call.communicate()[0]
+    call = sp.Popen(command.split(), stdout = sp.PIPE, stderr = sp.PIPE, shell = False)
+    output = call.communicate()
+    if call.wait() != 0:
+        raise Exception(output[0] or output[1])
+    return output[0]
 
 def convert(image, new_image, top, bottom):
     call("convert -strip -alpha off -colorspace RGB -depth 8 -trim -bordercolor white -border 1% \
@@ -15,25 +18,26 @@ def convert(image, new_image, top, bottom):
     call("convert -gravity north -crop 100%x5% " + new_image + " " + top)
     call("convert -gravity south -crop 100%x5% " + new_image + " " + bottom)
 
-def ocr(new_image, top, bottom):
+def ocr(image, new_image, top, bottom):
     call("tesseract " + new_image + " " + new_image)
     call("tesseract " + top + " " + top)
     call("tesseract " + bottom + " " + bottom)
 
-def grep(new_image, top, bottom):
+def grep(image, new_image, top, bottom):
     labels = call("grep -iE (fig|table) " + new_image + ".txt " + top + ".txt " + bottom + ".txt")
     for label in labels.split()[:1]:
         print "warning: " + label[:label.index(':')] + " contains label: " + label[label.index(':')+1:]
 
 def prepare(images):
+    if type(images) is not list:
+        raise Exception(images + ' is not a list. please supply a list of images')
     for image in images:
         new_image = image.replace('.eps', '.tif')
         top = new_image.replace('.tif', '_top.tif')
         bottom = new_image.replace('.tif', '_bottom.tif')
-
-        convert(image, new_image, top, bottom)
-        ocr(new_image, top, bottom)
-        grep(new_image, top, bottom)        
+        for step in [convert, ocr, grep]:
+            try: step(image, new_image, top, bottom)
+            except Exception as ee: print '** error in ' + step.__name__ + ': ' + str(ee)
 
 if __name__ == '__main__':
     if len(sys.argv) == 1:
